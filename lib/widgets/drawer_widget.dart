4af1b92933be87b798e853563499a1975ba1c967
Kushal <connect.kushal@gmail.com>
2019-12-29 19:55:52 +0530

    update  drawer header

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -73,46 +73,30 @@ class DrawerWidget extends StatelessWidget {
   }
 
   Widget drawerHeader() {
-    return DrawerHeader(
-      child: Column(
-        //crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      color: Colors.brown[100],
+      padding: EdgeInsets.all(1.0),
+      height: 150.0,
+
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Container(
-            padding: EdgeInsets.all(5.0),
-            child: Row(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                  height: 125.0,
-                  //width: 50.0,
-                  child: Image.asset("assets/images/cg_police_logo.png"),
-                ),
-                Flexible(
-                  fit: FlexFit.tight,
-                  child: Text(
-                    'Welcome to Mor Police',
-                    textAlign: TextAlign.center,
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                  ),
-                ),
-              ],
+            padding: EdgeInsets.only(top: 5.0, left: 5.0),
+            height: 125.0,
+            child: Image.asset("assets/images/cg_police_logo.png"),
+          ),
+          Flexible(
+            child: Text(
+              'Welcome to Mor Police',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
             ),
           ),
-
-          // CircleAvatar(
-          //   backgroundImage: AssetImage("assets/images/cg_police_logo.png"),
-          //   minRadius: 20.0,
-          //   maxRadius: 53.0,
-          // ),
-          // Image(
-          //   height: 100.0,
-          //   width: 100.0,
-          //   fit: BoxFit.fitWidth,
-          //   image: AssetImage(
-          //     'assets/profile_image.jpeg',
-          //   ),
-          // ),
         ],
       ),
     );
